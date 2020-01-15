e44bd73c4e960d18b0d870736f55a86e3d43f346
jannick vandaele <jannickvandaele@hotmail.com>
2020-01-15 19:50:11 +0100

    oud form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    errors: {}
+  };
+  handleSubmit = e => {
+    e.preventDefault(); // Probleem 1
+
+    const values = {
+      firstname: document.getElementById("firstname").value,
+      lastname: document.getElementById("lastname").value
+    }; // te veel werk
+
+    const errors = {};
+
+    if (!values.firstname) {
+      errors.firstname = "required";
+    }
+
+    if (!values.lastname) {
+      errors.lastname = "required";
+    }
+
+    // Object.keys(errors) => ["firstname", "lastname"] of []
+    if (Object.keys(errors).length > 0) {
+      this.setState({
+        errors: errors
+      });
+    } else {
+      // submit naar de server
+      this.setState({ errors: {} });
+      console.log(values);
+    }
+  };
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="container mt-4">
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                className={
+                  "form-control" + (errors.firstname ? " is-invalid" : "")
+                }
+                placeholder="First name"
+                id="firstname"
+              />
+              {errors.firstname && (
+                <div className="invalid-feedback">
+                  Please choose a firstname.
+                </div>
+              )}
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                className={
+                  "form-control" + (errors.lastname ? " is-invalid" : "")
+                }
+                placeholder="Last name"
+                id="lastname"
+              />
+              {errors.lastname && (
+                <div className="invalid-feedback">
+                  Please choose a lastname.
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col">
+              <input
+                type="submit"
+                value="GO!"
+                className="btn btn-primary btn-block"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
